5c27728d5255acb53f97293209a12ae55d0b2697
peroks <per.egil@roksvaag.com>
2024-10-27 00:45:22 +0200

    Rename class loaded action

--- inc/trait-singleton.php
+++ inc/trait-singleton.php
@@ -30,7 +30,7 @@ trait Singleton {
 
 		if ( empty( static::$inst[ $class ] ) ) {
 			static::$inst[ $class ] = apply_filters( Plugin::FILTER_CLASS_INSTANCE, new $class(), static::class );
-			do_action( Plugin::ACTION_CLASS_CREATED, static::$inst[ $class ], static::class );
+			do_action( Plugin::ACTION_CLASS_LOADED, static::$inst[ $class ], $class, static::class );
 		}
 		return static::$inst[ $class ];
 	}
